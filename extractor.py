f974a6cc97a45cb304d7ac7914466c8447fb1754
git2hub980 <useless311980@gmail.com>
2026-02-05 18:50:34 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,11 +1,10 @@
-# extractor.py
 import re
 
 def extract(message, intelligence):
-    upi = re.findall(r"[a-zA-Z0-9.\-_]{2,}@[a-zA-Z]{2,}", message)
-    phones = re.findall(r"\b\d{10}\b", message)
-    links = re.findall(r"https?://\S+", message)
+    intelligence.setdefault("upiIds", [])
+    intelligence.setdefault("phones", [])
+    intelligence.setdefault("links", [])
 
-    intelligence["upiIds"].extend(upi)
-    intelligence["phones"].extend(phones)
-    intelligence["links"].extend(links)
+    intelligence["upiIds"].extend(re.findall(r"[a-zA-Z0-9.\-_]{2,}@[a-zA-Z]{2,}", message))
+    intelligence["phones"].extend(re.findall(r"\b\d{10}\b", message))
+    intelligence["links"].extend(re.findall(r"https?://\S+", message))
