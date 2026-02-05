22ce2cea10b5b96d50fd838de797a2020cc31158
git2hub980 <useless311980@gmail.com>
2026-02-05 19:18:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Header, HTTPException
 from language_detector import detect_language
+from fastapi.responses import JSONResponse
 from dotenv import load_dotenv
 import os, requests
 
@@ -39,10 +40,18 @@ def honeypot(payload: dict, x_api_key: str = Header(None)):
 
     if confidence > 0.9 or session["messages"] > 18:
         send_final_callback(session_id, session)
-        return {
+        return JSONResponse(
+            content={
         "status": "success",
         "reply": agent_reply(session)
     }
+ )
+return JSONResponse(
+    content={
+        "status": "success",
+        "reply": agent_reply(session)
+    }
+)
 
 def send_final_callback(session_id, session):
     payload = {
