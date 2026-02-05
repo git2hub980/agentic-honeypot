24066fb6b488c4fa5d55e3edd04430398ce43195
git2hub980 <useless311980@gmail.com>
2026-02-05 19:31:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-from fastapi import FastAPI, Header, HTTPException
-from language_detector import detect_language
-from dotenv import load_dotenv
-import os, requests
-
-from scam_detector import progressive_confidence
-from sessions import get_session
-from extractor import extract
-from agesnt_logic import agent_reply
-from persona import choose_persona
-
-load_dotenv()
-app = FastAPI()
-
-API_KEY = os.getenv("API_KEY")
-
 @app.post("/honeypot")
 def honeypot(payload: dict, x_api_key: str = Header(None)):
     if x_api_key != API_KEY:
@@ -24,7 +8,7 @@ def honeypot(payload: dict, x_api_key: str = Header(None)):
 
     session = get_session(session_id)
     lang = detect_language(message)
-    session["language"]=lang
+    session["language"] = lang
 
     session["messages"] += 1
     confidence = progressive_confidence(message, session["history"])
@@ -40,27 +24,11 @@ def honeypot(payload: dict, x_api_key: str = Header(None)):
     if confidence > 0.9 or session["messages"] > 18:
         send_final_callback(session_id, session)
         return {
+            "status": "success",
+            "reply": agent_reply(session)
+        }
+
+    return {
         "status": "success",
         "reply": agent_reply(session)
-         }
- 
-return {
-        "status": "success",
-        "reply": agent_reply(session)
-}
-
-
-def send_final_callback(session_id, session):
-    payload = {
-        "sessionId": session_id,
-        "scamDetected": True,
-        "totalMessagesExchanged": session["messages"],
-        "extractedIntelligence": session["intelligence"],
-        "agentNotes": "Multi-persona honeypot with failure simulation & delay"
     }
-
-    requests.post(
-        "https://hackathon.guvi.in/api/updateHoneyPotFinalResult",
-        json=payload,
-        timeout=5
-    )
