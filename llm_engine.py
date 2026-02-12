f141910c6a9829ee51a3dfed2135d6784892c815
git2hub980 <useless311980@gmail.com>
2026-02-12 18:39:45 +0530

    Update llm_engine.py

--- llm_engine.py
+++ llm_engine.py
@@ -58,7 +58,7 @@ Reply realistically:
 """
 
     completion = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",
         messages=[
             {"role": "user", "content": prompt}
         ],
