c50381cc3868438c35cbb2b2dcc4b2bf1cee7c6c
git2hub980 <useless311980@gmail.com>
2026-02-12 14:25:41 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -1,24 +1,43 @@
 import json
 import numpy as np
 import faiss
-import os
 from sentence_transformers import SentenceTransformer
-
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-dataset_path = os.path.join(BASE_DIR, "scam_dataset.json")
-
-with open(dataset_path, "r", encoding="utf-8") as f:
+with open("scam_dataset.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
+texts = []
+clean_data = []
 
-texts = [item.get("fraudster", "") for item in data]
+for item in data:
+    # Case 1: simple structure
+    if "fraudster" in item:
+        texts.append(item["fraudster"])
+        clean_data.append(item)
 
+    elif "fraudster_message" in item:
+        texts.append(item["fraudster_message"])
+        clean_data.append({
+            "language": item["language"],
+            "fraudster": item["fraudster_message"],
+            "human_reply": item["human_reply"]
+        })
 
+    # Case 2: conversation list
+    elif "conversation" in item:
+        for convo in item["conversation"]:
+            fraud_msg = convo.get("fraudster") or convo.get("fraudster_message")
+            if fraud_msg:
+                texts.append(fraud_msg)
+                clean_data.append({
+                    "language": item["language"],
+                    "fraudster": fraud_msg,
+                    "human_reply": convo["human_reply"]
+                })
+
+# Generate embeddings
 embeddings = model.encode(texts)
 
 dimension = embeddings.shape[1]
@@ -30,17 +49,9 @@ def get_rag_reply(user_message, language):
     query_embedding = model.encode([user_message])
     distances, indices = index.search(query_embedding, k=3)
 
-    best_match = None
-
     for idx in indices[0]:
-     if data[idx]["language"] == language:
-        best_match = data[idx]
-        break
+        matched_item = clean_data[idx]
+        if matched_item["language"] == language:
+            return matched_item["human_reply"]
 
-    if not best_match:
-      return "Please wait..."
-
-    return best_match["human_reply"]
-
-
-    
+    return "Please wait..."
